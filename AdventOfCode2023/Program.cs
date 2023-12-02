e3f3e709f114a581a0bd4e056157aac0acaeab55
parkerreno <parkerreno@msn.com>
2023-12-01 21:33:34 -0800

    input folder, day 2

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -6,11 +6,12 @@
         {
             Console.WriteLine("Hello, World!");
             Day1();
+            Day2();
         }
 
         static void Day1()
         {
-            string[] lines = File.ReadAllLines("day1input.txt");
+            string[] lines = File.ReadAllLines("input/day1full.txt");
 
             int calibrationTotal = 0;
             foreach (string line in lines)
@@ -76,6 +77,82 @@
             }
 
             Console.WriteLine($"calibrationTotal: {calibrationTotal}");
+        }
+
+        static void Day2()
+        {
+            string[] raw = File.ReadAllLines("input/day2full.txt");
+
+            Dictionary<int, bool> gamePossible = new Dictionary<int, bool>();
+            Dictionary<int, int> gamePower = new Dictionary<int, int>(); // this didn't need to be a dictionary, could've summed the powers as we went, but it allowed for potential debugging of individual games
+            int possibleGamesSum = 0;
+            int powerSum = 0;
+
+            (int red, int green, int blue) possible = (12, 13, 14);
+            foreach (string rawGame in raw)
+            {
+                string[] idAndData = rawGame.Split(":");
+                int gameId = int.Parse(idAndData[0].Split(" ")[1]);
+
+                int minRed = 0;
+                int minGreen = 0;
+                int minBlue = 0;
+
+                string[] gameRolls = idAndData[1].Split(";");
+                foreach (string gameRoll in gameRolls)
+                {
+                    string[] rollData = gameRoll.Split(",");
+                    // This is a bit silly and could almost certainly be done better, but it works
+                    int red = int.Parse(rollData.Where(x => x.Contains("red"))?.FirstOrDefault()?.Split(" ")[1] ?? "0");
+                    int green = int.Parse(rollData.Where(x => x.Contains("green"))?.FirstOrDefault()?.Split(" ")[1] ?? "0");
+                    int blue = int.Parse(rollData.Where(x => x.Contains("blue"))?.FirstOrDefault()?.Split(" ")[1] ?? "0");
+
+                    if (red <= possible.red && green <= possible.green && blue <= possible.blue)
+                    {
+                        // try add rather and no overwrite - it will either already be true or if it's false, we want it to stay false
+                        gamePossible.TryAdd(gameId, true);
+                    }
+                    else
+                    {
+                        // I think we could probably just do the stuff in the if block, but this is probably better practice?  and I already wrote it
+                        if (!gamePossible.TryAdd(gameId, false))
+                        {
+                            gamePossible[gameId] = false;
+                        }
+                    }
+
+                    if (red > minRed)
+                    {
+                        minRed = red;
+                    }
+                    if (green > minGreen)
+                    {
+                        minGreen = green;
+                    }
+                    if (blue > minBlue)
+                    {
+                        minBlue = blue;
+                    }
+                }
+
+                gamePower.Add(gameId, minRed * minGreen * minBlue);
+            }
+
+            foreach (KeyValuePair<int, bool> game in gamePossible)
+            {
+                if (game.Value)
+                {
+                    possibleGamesSum += game.Key;
+                }
+            }
+
+            foreach (KeyValuePair<int, int> game in gamePower)
+            {
+                powerSum += game.Value;
+            }
+
+            Console.WriteLine($"possibleGamesSum: {possibleGamesSum}");
+            Console.WriteLine($"powerSum: {powerSum}");
         }
     }
 }
